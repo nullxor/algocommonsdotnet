a80d9355091a04248d22f66b3c68af3d4711b3b8
Olfran Jiménez <olfran@gmail.com>
2016-11-28 21:31:47 -0400

    Added MinHeap unit test.

---
+++ src/AlgoCommonsTest/DataStructures/Generic/Trees/MinHeapTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgoCommonsDotNet.DataStructures.Generic.Trees;
+using System.Linq;
+
+namespace AlgoCommonsTest.DataStructures.Generic.Trees
+{
+    [TestClass]
+    public class MinHeapTests
+    {
+        HeapBase<int> _heap;
+        int[] _array;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            _heap = new MinHeap<int>(_array);
+        }
+
+        [TestMethod]
+        public void ShouldReturnTheMinElementAfterRemoveTop()
+        {
+            var array = _heap.ToArray();
+            var min = _heap.RemoveTop();
+            Assert.AreEqual(array.Min(), min);
+        }
+    }
+}
