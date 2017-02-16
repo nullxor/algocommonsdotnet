c219a36bef9196e0503c769a0e775d7258e66734
Olfran Jiménez <olfran@gmail.com>
2017-02-16 16:25:17 -0400

    Extend the BinaryTreeNode class by adding a color flag for Red Black Trees

---
+++ src/AlgoCommonsDotNet/DataStructures/Generic/Trees/RedBlackTreeNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AlgoCommonsDotNet.DataStructures.Generic.Trees
+{
+    /// <summary>
+    /// A Node for a Binary Tree
+    /// </summary>
+    public class RedBlackTreeNode <K,V> : BinaryTreeNode<K,V> where K : IComparable<K>
+    {
+        /// <summary>
+        /// Is a black or red node, false by default, new nodes should be red
+        /// </summary>
+        public bool IsBlack { get; set; }
+
+        public RedBlackTreeNode(BinaryTreeNode<K,V> parent, BinaryTreeNode<K,V> left, 
+            BinaryTreeNode<K,V> right, K key, V value, bool isBlack = false) :
+        base(parent, left, right, key, value)
+
+        {
+            IsBlack = isBlack;
+        }
+    }
+}
+
